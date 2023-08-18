104a3fdbee12e4efbd0d4ea968c58f97592187c7
adarsh <adarshshanbhag5@gmail.com>
2023-08-19 03:07:37 +0530

    feat: implement palette api to native module
    This commit implement the palette api which gets prominant colors from given image
    
    native module is backward compatible

--- android/src/main/java/com/palettepicker/PalettePickerModule.java
+++ android/src/main/java/com/palettepicker/PalettePickerModule.java
@@ -1,5 +1,6 @@
 package com.palettepicker;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -17,7 +18,6 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
 
 import java.io.IOException;
@@ -42,7 +42,7 @@ public class PalettePickerModule extends PalettePickerSpec {
 
   private static final Pattern HEX_PATTERN = Pattern.compile("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$");
 
-  private static String parseFallbackColor(String hex) {
+  private String parseFallbackColor(String hex) {
     Matcher matcher = HEX_PATTERN.matcher(hex);
 
     if (!matcher.matches()) {
@@ -56,7 +56,7 @@ public class PalettePickerModule extends PalettePickerSpec {
     return "#" + hex.charAt(1) + hex.charAt(1) + hex.charAt(2) + hex.charAt(2) + hex.charAt(3) + hex.charAt(3);
   }
 
-  private static String getHex(int rgb) {
+  private String getHex(int rgb) {
     return String.format("#%06X", 0xFFFFFF & rgb);
   }
 
@@ -64,92 +64,95 @@ public class PalettePickerModule extends PalettePickerSpec {
     promise.reject("[color-palette]", e.getMessage(), e);
   }
 
-  public WritableMap createPaletteAsync(Bitmap bitmap, int fallbackColorInt, String fallbackColor, String fallbackTextColor) {
-    WritableMap colorPalette = Arguments.createMap();
-    Palette.from(bitmap).generate(palette -> {
-      if (palette != null) {
-        Palette.Swatch dominant = palette.getDominantSwatch();
-        if (dominant != null) {
-          colorPalette.putString("dominant", getHex(dominant.getRgb()));
-          colorPalette.putString("titleTextColor", getHex(dominant.getTitleTextColor()));
-          colorPalette.putString("bodyTextColor", getHex(dominant.getBodyTextColor()));
-        } else {
-          colorPalette.putString("dominant", fallbackColor);
-          colorPalette.putString("titleTextColor", fallbackTextColor);
-          colorPalette.putString("bodyTextColor", fallbackTextColor);
-        }
-        colorPalette.putString("vibrant", getHex(palette.getVibrantColor(fallbackColorInt)));
-        colorPalette.putString("darkVibrant", getHex(palette.getDarkVibrantColor(fallbackColorInt)));
-        colorPalette.putString("lightVibrant", getHex(palette.getLightVibrantColor(fallbackColorInt)));
-        colorPalette.putString("muted", getHex(palette.getMutedColor(fallbackColorInt)));
-        colorPalette.putString("darkMuted", getHex(palette.getDarkMutedColor(fallbackColorInt)));
-        colorPalette.putString("lightMuted", getHex(palette.getLightMutedColor(fallbackColorInt)));
-      } else {
-        colorPalette.putString("vibrant", fallbackColor);
-        colorPalette.putString("darkVibrant", fallbackColor);
-        colorPalette.putString("lightVibrant", fallbackColor);
-        colorPalette.putString("muted", fallbackColor);
-        colorPalette.putString("darkMuted", fallbackColor);
-        colorPalette.putString("lightMuted", fallbackColor);
+  private Bitmap imageUriToBitmap(String uri) throws IOException {
+
+    //processing image uri
+    Bitmap image = null;
+
+    Context context = getReactApplicationContext();
+
+    @SuppressLint("DiscouragedApi") int resourceId = context.getResources().getIdentifier(uri, "drawable", context.getPackageName());
+    // Check if local resource
+    if (resourceId != 0) {
+      image = BitmapFactory.decodeResource(context.getResources(), resourceId);
+    }
+
+    // Check if base64
+    if (uri.startsWith("data:image")) {
+      String base64Uri = uri.split(",")[1];
+//      Log.d("base64", base64Uri);
+      byte[] decodedBytes = Base64.decode(base64Uri, Base64.DEFAULT);
+
+      image = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+    }
+
+    //check if internal storage (requires appropriate permission)
+    if (uri.startsWith("file://")) {
+      String imgUri = Uri.parse(uri).getPath();
+//        Log.d("local",imgUri);
+      image = BitmapFactory.decodeFile(imgUri);
+    }
+
+    // check if url (link)
+    if (URLUtil.isValidUrl(uri)) {
+      URI parsedUri = URI.create(uri);
+//      Log.d("link", parsedUri.toString());
+      HttpURLConnection connection = (HttpURLConnection) parsedUri.toURL().openConnection();
+
+      try (InputStream inputStream = connection.getInputStream()) {
+        image = BitmapFactory.decodeStream(inputStream);
       }
+    }
 
-    });
+    if (image == null) {
+      throw new IOException("Failed to get image");
+    }
 
-    return colorPalette;
+    return image;
   }
 
 
   @ReactMethod
-  public void getPalette(ReadableMap resource, ReadableMap config, Promise promise) {
+  public void getPalette(String uri, ReadableMap config, Promise promise) {
     try {
       String fallbackTextColor = config.getString("fallbackTextColor");
       String fallbackColor = parseFallbackColor(config.getString("fallback"));
       int fallbackColorInt = Color.parseColor(fallbackColor);
 
-      //processing image uri
-      Bitmap image = null;
-      Context context = getReactApplicationContext();
+      Bitmap bitmap = imageUriToBitmap(uri);
 
-      int resourceId = resource.getInt("resourceId");
-      String uri = resource.getString("uri");
+      Palette.from(bitmap).generate(palette -> {
+        WritableMap colorPalette = Arguments.createMap();
+        if (palette != null) {
+          Palette.Swatch dominant = palette.getDominantSwatch();
+          if (dominant != null) {
+            colorPalette.putString("dominant", getHex(dominant.getRgb()));
+            colorPalette.putString("titleTextColor", getHex(dominant.getTitleTextColor()));
+            colorPalette.putString("bodyTextColor", getHex(dominant.getBodyTextColor()));
 
-      // Check if local resource (required from react native)
-      if (resourceId != 0) {
-        image = BitmapFactory.decodeResource(context.getResources(), resourceId);
-      }
-
-      if (resourceId == 0 && uri != null) {
-        // Check if base64
-        if (uri.startsWith("data:image")) {
-          String base64Uri = uri.split(",")[1];
-          byte[] decodedBytes = Base64.decode(base64Uri, Base64.DEFAULT);
-
-          image = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
-        }
-
-        //check if internal storage (requires appropriate permission)
-        if (uri.startsWith("file://")) {
-          String imgUri = Uri.parse(uri).getPath();
-          image = BitmapFactory.decodeFile(imgUri);
-        }
-
-        // check if url (link)
-        if (URLUtil.isValidUrl(uri)) {
-          URI parsedUri = URI.create(uri);
-          HttpURLConnection connection = (HttpURLConnection) parsedUri.toURL().openConnection();
-          
-          try (InputStream inputStream = connection.getInputStream()) {
-            image = BitmapFactory.decodeStream(inputStream);
+          } else {
+            colorPalette.putString("dominant", fallbackColor);
+            colorPalette.putString("titleTextColor", fallbackTextColor);
+            colorPalette.putString("bodyTextColor", fallbackTextColor);
           }
-        }
-      }
+          colorPalette.putString("vibrant", getHex(palette.getVibrantColor(fallbackColorInt)));
+          colorPalette.putString("darkVibrant", getHex(palette.getDarkVibrantColor(fallbackColorInt)));
+          colorPalette.putString("lightVibrant", getHex(palette.getLightVibrantColor(fallbackColorInt)));
+          colorPalette.putString("muted", getHex(palette.getMutedColor(fallbackColorInt)));
+          colorPalette.putString("darkMuted", getHex(palette.getDarkMutedColor(fallbackColorInt)));
+          colorPalette.putString("lightMuted", getHex(palette.getLightMutedColor(fallbackColorInt)));
 
-      if (image == null) {
-        throw new IOException("Failed to get image");
-      } else {
-        WritableMap colorPalette = createPaletteAsync(image, fallbackColorInt, fallbackColor, fallbackTextColor);
+        } else {
+          colorPalette.putString("vibrant", fallbackColor);
+          colorPalette.putString("darkVibrant", fallbackColor);
+          colorPalette.putString("lightVibrant", fallbackColor);
+          colorPalette.putString("muted", fallbackColor);
+          colorPalette.putString("darkMuted", fallbackColor);
+          colorPalette.putString("lightMuted", fallbackColor);
+        }
         promise.resolve(colorPalette);
-      }
+      });
+
     } catch (Exception e) {
       handleError(promise, e);
     }
--- android/src/oldarch/PalettePickerSpec.java
+++ android/src/oldarch/PalettePickerSpec.java
@@ -3,11 +3,12 @@ package com.palettepicker;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReadableMap;
 
 abstract class PalettePickerSpec extends ReactContextBaseJavaModule {
   PalettePickerSpec(ReactApplicationContext context) {
     super(context);
   }
 
-  public abstract void getPalette(ReadableMap resource, ReadableMap config, Promise promise);
+  public abstract void getPalette(String uri, ReadableMap config, Promise promise);
 }
